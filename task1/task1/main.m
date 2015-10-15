6f12d132222f56424fc6d67c602f85a979d8d161
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-10-16 00:39:20 +0300

    declaration primitive data types

--- task1/task1/main.m
+++ task1/task1/main.m
@@ -6,12 +6,24 @@
 //  Copyright © 2015 geekhub. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "UIKit/UIKit.h"
 
 int main(int argc, char * argv[]) {
-	// Task 1
-	
-	
+	// Task 1 - Basic types
+    int integerVariable1 = -5;
+    unsigned int integerVariable2 = 1;
+    NSInteger integerVariable3 = 15;
+    
+    float floatVariable1 = .8f;
+    CGFloat floatVariable2 = 5.2f;
+    
+    double doubleVariable1 = 5.432;
+    long double pi = 3.14159265358979323846;
+    
+    NSString *stringVariable1 = @"String";
+    char charVariable1 = 'x';    
+    
 	// Task 2
 	
 	
